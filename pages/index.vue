23ef0a6617f7a60d5ddf1ddf1918b69a692c523d
neeoon <noeajuro@gmail.com>
2021-05-15 14:27:40 -0500

    [Add] Papa parse package [Add] Parse CSV to JSON data

--- pages/index.vue
+++ pages/index.vue
@@ -25,51 +25,57 @@
                   >
                   </VueFileAgent>
                </v-row>
-               <v-row justify="center">
-                  <v-btn color="success" @click="uploadFiles()">Subir archivo</v-btn>
-               </v-row>
             </client-only>
+            <v-row justify="center">
+               Data {{ dataFromFile }}
+            </v-row>
         </v-col>
     </v-row>
 </template>
 
 <script>
-  export default {
-    layout: 'main',
-    data() {
-      return {
-         fileRecords: [],
-         snackbar: false,
-         snackbarMessage: '',
-      };
-    },
-    methods: {
-      uploadFiles: function () {
-         if(this.fileRecords[0].ext == 'csv') {
-            console.log("File", this.fileRecords);
-         } else {
-            this.fileRecords = [];
-            this.snackbar = true;
-            this.snackbarMessage = 'El archivo debe ser .CSV';
-         }
+   import Papa from 'papaparse'
+   export default {
+      layout: 'main',
+      data() {
+         return {
+            fileRecords: [],
+            snackbar: false,
+            snackbarMessage: '',
+            dataFromFile: null,
+         };
       },
-      filesSelected: function (fileRecordsNewlySelected) {
-         if(this.fileRecords[0].ext == 'csv') {
-            console.log("File", this.fileRecords);
-         } else {
-            this.fileRecords = [];
-            this.snackbar = true;
-            this.snackbarMessage = 'El archivo debe ser .CSV';
-         }
+      methods: {
+         filesSelected: function (fileRecordsNewlySelected) {
+            if(this.fileRecords[0].ext == 'csv') {
+               if (process.env.NODE_ENV == 'development')
+                  console.log("File", this.fileRecords[0].file);
+
+               var that = this;
+
+               Papa.parse(this.fileRecords[0].file, {
+                  header: true,
+                  complete: function (results) {
+                        console.log("DATA FILE: ", results);
+                        that.dataFromFile = results.data;
+                        that.fields = results.fields;
+                        console.log("DATA FILE VUE: ", that.dataFromFile);
+                  }
+               });
+            } else {
+               this.fileRecords = [];
+               this.snackbar = true;
+               this.snackbarMessage = 'El archivo debe ser .CSV';
+            }
+         },
+         onBeforeDelete: function (fileRecord) {
+         this.fileRecords = [];
+         },
+         fileDeleted: function (fileRecord) {
+         this.fileRecords = [];
+         },
       },
-      onBeforeDelete: function (fileRecord) {
-        this.fileRecords = [];
-      },
-      fileDeleted: function (fileRecord) {
-        this.fileRecords = [];
-      },
-   },
-};
+   };
 </script>
 
 <style scoped>
@@ -79,8 +85,13 @@
 <style>
    #profile-pic-demo .drop-help-text { display: none; }
    #profile-pic-demo .is-drag-over .drop-help-text { display: block; }
-   #profile-pic-demo .profile-pic-upload-block { border: 2px dashed transparent; padding: 20px; padding-top: 0; width: 90%; max-width: 500px; }
-   #profile-pic-demo .is-drag-over.profile-pic-upload-block { border-color: #AAA; }
+   #profile-pic-demo .profile-pic-upload-block { border: 0; background-color: cadetblue; padding: 0; color: white; width: 90%; max-width: 600px; }
+   #profile-pic-demo .is-drag-over.profile-pic-upload-block { border-color: transparent; }
    #profile-pic-demo .vue-file-agent { width: 100%; float: left; border: 0; box-shadow: none; }
+   .file-preview svg { fill: white !important; margin-top: 30% !important; height: 26% !important }
+   .file-preview span { color: white !important; border-color: transparent; font-size: 25px !important; line-height: normal !important; height: auto !important; }
+   .vue-file-agent .file-preview-new { padding: 0 !important; }
+   .vue-file-agent .file-preview .file-delete { top: 5px; right: 15px !important }
+   .vue-file-agent .is-deletable .file-preview .file-name { padding-top: 10px }
 </style>
 
